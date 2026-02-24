a13e6af54d2a2f7c7da42c6baf5a62b3604015d1
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-02-24 15:20:23 +0800

    Update .gitignore, add dependencies, and implement AprilTag detection functionality

---
+++ src/checkerboard_cli/config/ChessMapping.py
@@ -0,0 +1,14 @@
+ChessMapping = {
+    1 : "P",  # White Pawn
+    2 : "N",  # White Knight
+    3 : "B",  # White Bishop
+    4 : "R",  # White Rook
+    5 : "Q",  # White Queen
+    6 : "K",  # White King
+    7 : "p",  # Black Pawn
+    8 : "n",  # Black Knight
+    9 : "b",  # Black Bishop
+    10 : "r", # Black Rook
+    11 : "q", # Black Queen
+    12 : "k", # Black King
+}
---
+++ src/checkerboard_cli/getPosition.py
@@ -0,0 +1,65 @@
+import cv2
+from pupil_apriltags import Detector
+from checkerboard_cli.config.ChessMapping import ChessMapping
+from PIL import Image, ImageDraw, ImageFont
+
+
+def getFrame(cap):
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        return None
+    ret, frame = cap.read()
+    cap.release()
+    if not ret:
+        print("Error: Could not read frame from camera.")
+        return None
+    return frame
+
+
+def detectAprilTags(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detector = Detector()
+    tags = detector.detect(gray)
+    return tags
+
+
+def getCapture(camera_index=0):
+    cap = cv2.VideoCapture(camera_index)
+    frame = getFrame(cap)
+    if frame is None:
+        return None
+    tags = detectAprilTags(frame)
+    if not tags:
+        print("No AprilTags detected.")
+    
+    shortest_distance = float('inf')
+
+    for tag in tags:
+        print(f"Detected tag ID: {tag.tag_id} at position: {tag.center}")
+        # draw the tag on the frame
+        corners = tag.corners.astype(int)
+        symbol = ChessMapping.get(tag.tag_id, str(tag.tag_id))
+        print(f"Mapping tag ID {tag.tag_id} to symbol: {symbol}")
+
+        # if symbol is Upper case, draw and fill in White, else draw in Black
+        fill_color = (255, 255, 255) if symbol.isupper() else (0, 0, 0)
+        text_color = (0, 0, 0) if symbol.isupper() else (255, 255, 255)
+        for i in range(4):
+            # cv2.line(frame, tuple(corners[i]), tuple(corners[(i + 1) % 4]), (255, 0, 255), 2)
+            if corners[i][1] < shortest_distance:
+                shortest_distance = corners[i][1]
+        # fill the tag with the color
+        cv2.fillPoly(frame, [corners], fill_color)
+        
+        font_scale = shortest_distance / 200.0
+        # draw the tag ID near the center
+        cv2.putText(frame, symbol, (int(tag.center[0] - 10 * font_scale), int(tag.center[1] + 10 * font_scale)), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, 5)
+
+    return frame
+
+
+if __name__ == "__main__":
+    # list cv2 video capture devices and detail
+    print("Available video capture devices:")
+    frame = getCapture(1)
+    cv2.imwrite("detected_tags.jpg", frame)
