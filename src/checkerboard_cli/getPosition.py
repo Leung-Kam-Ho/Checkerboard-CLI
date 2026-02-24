3249be3e55b93fb576c23a103422378e6bc6baad
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-02-24 16:02:25 +0800

    Add numpy import and enhance tag visualization in getPosition.py

--- src/checkerboard_cli/getPosition.py
+++ src/checkerboard_cli/getPosition.py
@@ -2,6 +2,7 @@ import cv2
 from pupil_apriltags import Detector
 from checkerboard_cli.config.ChessMapping import ChessMapping
 import logging
+import numpy as np
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -35,7 +36,7 @@ def getCapture(camera_index=0):
     if not tags:
         logger.warning("No AprilTags detected.")
     
-    shortest_distance = float('inf')
+    
 
     for tag in tags:
         logger.debug(f"Detected tag ID: {tag.tag_id} at position: {tag.center}")
@@ -43,18 +44,19 @@ def getCapture(camera_index=0):
         corners = tag.corners.astype(int)
         symbol = ChessMapping.get(tag.tag_id, str(tag.tag_id))
         logger.debug(f"Mapping tag ID {tag.tag_id} to symbol: {symbol}")
+        
+        print(tag)
 
         # if symbol is Upper case, draw and fill in White, else draw in Black
         fill_color = (255, 255, 255) if symbol.isupper() else (0, 0, 0)
         text_color = (0, 0, 0) if symbol.isupper() else (255, 255, 255)
-        for i in range(4):
-            # cv2.line(frame, tuple(corners[i]), tuple(corners[(i + 1) % 4]), (255, 0, 255), 2)
-            if corners[i][1] < shortest_distance:
-                shortest_distance = corners[i][1]
+        shortest_distance = corners[1][0] - corners[0][0]
         # fill the tag with the color
-        cv2.fillPoly(frame, [corners], fill_color)
+        # cv2.fillPoly(frame, [corners], fill_color)
+        # draw a circle that cover the polygon
+        cv2.circle(frame, (int(tag.center[0]), int(tag.center[1])), int(np.sqrt(2 * (shortest_distance ** 2)) / 1.95), fill_color, -1)  # diameter^2 = a^2 + b^2, a = b = shortest_distance, divide by 1.95 to avoid rounding error that cause the rectangle of the tag to be visible
         
-        font_scale = shortest_distance / 200.0
+        font_scale = shortest_distance / 50.0
         # draw the tag ID near the center
         cv2.putText(frame, symbol, (int(tag.center[0] - 10 * font_scale), int(tag.center[1] + 10 * font_scale)), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, 5)
 
